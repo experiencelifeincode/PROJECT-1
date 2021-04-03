48fa8d8cd72a27014b063b8749cd91e99b054b0c
Kendra Wing <kendracwing@icloud.com>
2021-04-03 12:45:27 -0600

    Edited all files attempting to upload

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,77 @@
 //get the api keys from the websites I want to use
-//api key= AIzaSyDFJN8KsV0vonVDnuMQg39pedzZxhh7rVs
+//api key= AIzaSyAW220mOd1otBnP8lwhTJXTxRzMby1M2PE
 //above is for geolocation api https://www.googleapis.com/geolocation/v1/geolocate?key=YOUR_API_KEY
+
+var showEl = document.getElementById('searchBtn');
 
 
 let map;
 
+var searchBtn = document.getElementById('searchBtn');
+
+
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+
+        center: { lat: 44.6969444, lng: -118.0186111 },
+        zoom: 12,
+
     });
+}
+// document.getElementById('searchBtn').addEventListener("click", function (){
+//  showMap();
+// })
+
+function showMap() {
+    document.getElementByID('map').style.display = 'show';
 }
 
 // function to start the map location from google maps api
-let venueMap;
-function init() {
-    var mapOptions = {                        // set up the map options
-        center: new google.maps.LatLng("0.0", "0.0"),
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoom: 13
-    };
-    //Map() draws a map
-    venueMap = new google.maps.Map(document.getElementById('mapDiv'), mapOptions);
+// let venueMap; 
+// function init() {
+//   var mapOptions = {                         // set up the map options
+//     center: new google.maps.LatLng("0.0", "0.0"),
+//     mapTypeId: google.maps.MapTypeId.ROADMAP,
+//     zoom: 13
+//   };
+//                                       //Map() draws a map
+//   venueMap = new google.maps.Map(document.getElementById('mapDiv'), mapOptions);
+// }
+
+// function loadScript() {
+//   var script = document.createElement('script');                           //Create <script> element
+//   script.src = 'http://maps.googleapis.com/maps/api/js?key=AIzaSyAW220mOd1otBnP8lwhTJXTxRzMby1M2PE&callback=init';
+
+//   var mapDiv = document.getElementById("mapDiv");
+//   mapDiv.appendChild(script); //add element to page
+// };
+
+
+
+// function loadScript() {
+//   var script = document.createElement('script');                           //Create <script> element
+//   script.src = 'http://maps.googleapis.com/maps/api/js?key=AIzaSyAW220mOd1otBnP8lwhTJXTxRzMby1M2PE&callback=init';
+
+//   var mapDiv = document.getElementById("mapDiv");
+//   mapDiv.appendChild(script); //add element to page
+// };
+
+
+// window.onload = loadScript; //Onload call
+
+
+showEl.addEventListener('click', function () {
+    showMap();
+})
+
+// window.onload = loadScript; //Onload call
+
+
+function showMap() {
+
+    document.getElementById('map').style.display = 'block';
 }
-
-function loadScript() {
-    var script = document.createElement('script');                           //Create <script> element
-    script.src = 'http://maps.googleapis.com/maps/api/js?key=AIzaSyDFJN8KsV0vonVDnuMQg39pedzZxhh7rVs&callback=init';
-
-    var mapDiv = document.getElementById("mapDiv");
-    mapDiv.appendChild(script); //add element to page
-};
-
-
-window.onload = loadScript; //Onload call
-
 
 //Random Quote Generator
 $(document).ready(function () {
@@ -78,29 +115,3 @@ $(document).ready(function () {
     getQuote();
 });
 
-
-//PLACEHOLDER Drop Down List
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("campgroundlist").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
-
-$(document).ready(function () {
-    $('input[rel="txtTooltip"]').tooltip();
-});
